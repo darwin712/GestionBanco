0c7f8d3ac48c5ffdd26fdc66eebee6e2265ddfb2
Diego-Torres-G <diego.torres.garcia@uabc.edu.mx>
2025-11-13 18:17:00 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("prueba");
+        System.out.println("No conozco una actividad de menor retorno de la inversion");
     }
 }
